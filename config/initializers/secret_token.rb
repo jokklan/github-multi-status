363b39890366856128fd57f1d2be8e260824ab96
Johan Frølich <johanfrolich@gmail.com>
2014-03-06 12:25:14 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MultiStatus::Application.config.secret_key_base = 'ab0e1d76aacc6d463b7b8bf29c3088debb140e4b35b6c9601d11ac6836e0772f03fc54f32edf5406665db58fe0c74e8068568e38830f175d6f0b655ccabc029d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MultiStatus::Application.config.session_store :cookie_store, key: '_multi_status_session'
